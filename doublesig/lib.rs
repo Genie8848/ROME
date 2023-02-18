292a804bc780ccbbdf402e568a4ad20daf72ae3c
Kofi Otuo <otuokofi@outlook.com>
2023-02-18 20:33:43 +0000

    Fill functions 2

--- doublesig/lib.rs
+++ doublesig/lib.rs
@@ -34,7 +34,7 @@ mod doublesig {
     use ink::codegen::Env;
 
     #[ink(storage)]
-    pub struct Doublesig {
+    pub struct DoubleSig {
         user: AccountId,
         expiration: Timestamp,
         // store amount held as a decimal
@@ -47,14 +47,21 @@ mod doublesig {
     pub enum Error {
         /// Returned if caller is not the `sender` while required to.
         NotYetExpired,
-        InsufficientFunds,
+        CallerIsNotOwner,
+        InsufficientFunds {
+            total_balance: Balance,
+            /// refers to the contract amount left after potential deduction
+            potential_balance: Balance,
+            funds_to_transfer: Balance,
+            existential_deposit: Balance,
+        },
         TransferAmountTooLarge,
     }
 
     /// Type alias for the contract's `Result` type.
     pub type Result<T> = core::result::Result<T, Error>;
 
-    impl Doublesig {
+    impl DoubleSig {
         /// Creates a new instance of this contract.
         /// `expiration` refers to how long you'd, like to keep your funds in this contract.
         /// After the expiration period, you are allowed to withdraw the funds.
@@ -77,14 +84,26 @@ mod doublesig {
                 if amount > f64::MAX as Balance {
                     return Err(Error::TransferAmountTooLarge);
                 };
-                amount as f64 * 0.03
+                amount as f64 * 0.03 // 3%
             };
+            println!("deduct {}", amount_to_deduct);
+            println!(
+                "testion {:?}",
+                balance.checked_sub(self.env().minimum_balance())
+            );
             let current_balance = balance
                 .checked_sub(self.env().minimum_balance())
                 .and_then(|res| res.checked_sub(amount_to_deduct.ceil() as Balance))
                 .unwrap_or_default();
-            if current_balance < self.amount_held {
-                return Err(Error::InsufficientFunds);
+            println!("current {}", current_balance);
+            if current_balance <= amount {
+                println!("inner {} cuu {}", self.amount_held, current_balance);
+                return Err(Error::InsufficientFunds {
+                    total_balance: self.get_balance(),
+                    funds_to_transfer: amount,
+                    potential_balance: current_balance,
+                    existential_deposit: self.env().minimum_balance(),
+                });
             }
             if self.env().transfer(destination, amount).is_err() {
                 panic!(
@@ -100,8 +119,10 @@ mod doublesig {
         /// Withdraw all funds in the contract and terminate the contract.
         /// This returns an error when the expiration date has not reached
         #[ink(message)]
-        pub fn withdraw_stored_funds(&mut self) -> Result<()> {
-            //
+        pub fn claim_funds(&mut self) -> Result<()> {
+            if self.env().caller() != self.user {
+                return Err(Error::CallerIsNotOwner);
+            }
             let now = self.env().block_timestamp();
             if now < self.expiration {
                 return Err(Error::NotYetExpired);
@@ -109,10 +130,57 @@ mod doublesig {
             self.env().terminate_contract(self.user);
         }
 
-        /// Returns the `balance` of the contract.
+        /// Transfer savings to `senders` account
+        pub fn withdraw_savings(&mut self) -> Result<()> {
+            if self.env().caller() != self.user {
+                return Err(Error::CallerIsNotOwner);
+            }
+            let now = self.env().block_timestamp();
+            if now < self.expiration {
+                return Err(Error::NotYetExpired);
+            }
+            if self
+                .env()
+                .transfer(self.env().caller(), self.amount_held)
+                .map(|_| self.amount_held = 0)
+                .is_err()
+            {
+                panic!(
+                    "requested transfer failed. this can be the case if the contract does not\
+                     have sufficient free funds or if the transfer would have brought the\
+                     contract's balance below minimum balance."
+                )
+            }
+            Ok(())
+        }
+
+        /// Get the current spendable amount (`free balance`)
+        #[ink(message)]
+        pub fn free(&self) -> Balance {
+            println!("min {}", self.env().minimum_balance());
+            self.get_balance()
+                .checked_sub(self.env().minimum_balance())
+                .and_then(|res| res.checked_sub(self.amount_held))
+                .unwrap_or_default()
+        }
+
+        /// Get the total value of the funds which has been saved
+        // This amount should never decrease
+        #[ink(message)]
+        pub fn amount_stored(&self) -> Balance {
+            self.amount_held
+        }
+
+        /// Returns the total `balance` of the contract.
         #[ink(message)]
         pub fn get_balance(&self) -> Balance {
             self.env().balance()
+        }
+
+        /// Returns the `expiration` of the contract.
+        #[ink(message)]
+        pub fn get_expiration(&self) -> Timestamp {
+            self.expiration
         }
     }
 
@@ -121,6 +189,133 @@ mod doublesig {
         /// Imports all the definitions from the outer scope so we can use them here.
         use super::*;
 
-        //TODO
+        fn default_accounts() -> ink::env::test::DefaultAccounts<ink::env::DefaultEnvironment> {
+            ink::env::test::default_accounts::<ink::env::DefaultEnvironment>()
+        }
+
+        fn get_account_balance(account: AccountId) -> Balance {
+            ink::env::test::get_account_balance::<ink::env::DefaultEnvironment>(account)
+                .expect("Cannot get account balance")
+        }
+
+        fn get_current_time() -> Timestamp {
+            let since_the_epoch = std::time::SystemTime::now()
+                .duration_since(std::time::UNIX_EPOCH)
+                .expect("Time went backwards");
+            since_the_epoch.as_secs() + since_the_epoch.subsec_nanos() as u64 / 1_000_000_000
+        }
+
+        fn set_balance(account_id: AccountId, balance: Balance) {
+            ink::env::test::set_account_balance::<ink::env::DefaultEnvironment>(account_id, balance)
+        }
+
+        fn set_sender(sender: AccountId) {
+            ink::env::test::set_caller::<ink::env::DefaultEnvironment>(sender);
+        }
+
+        fn get_balance(account_id: AccountId) -> Balance {
+            ink::env::test::get_account_balance::<ink::env::DefaultEnvironment>(account_id)
+                .expect("Cannot get account balance")
+        }
+
+        fn contract_id() -> AccountId {
+            ink::env::test::callee::<ink::env::DefaultEnvironment>()
+        }
+
+        fn create_contract(initial_balance: Balance) -> DoubleSig {
+            let accounts = default_accounts();
+            set_sender(accounts.alice);
+            set_balance(contract_id(), initial_balance);
+            DoubleSig::new(1000)
+        }
+
+        fn advance_block() {
+            ink::env::test::advance_block::<ink::env::DefaultEnvironment>();
+        }
+
+        #[ink::test]
+        fn test_transfer_works() {
+            let contract_balance = 100_000_000;
+            let accounts = default_accounts();
+            let mut smart_contract = create_contract(contract_balance);
+            set_sender(accounts.eve);
+            set_balance(accounts.eve, 0);
+            smart_contract
+                .transfer_funds(accounts.eve, 2_000_000)
+                .unwrap();
+            println!("hi tejre {}", smart_contract.free());
+            println!("hi tejre {}", smart_contract.amount_stored());
+            println!("hi tejre {}", smart_contract.get_balance());
+            assert_eq!(get_balance(accounts.eve), 2_000_000);
+        }
+
+        #[ink::test]
+        fn test_deduction_works() {
+            let contract_balance = 100_000_000;
+            let accounts = default_accounts();
+            let mut smart_contract = create_contract(contract_balance);
+            // send initial funds
+            set_sender(accounts.eve);
+            set_balance(accounts.eve, 0);
+            smart_contract
+                .transfer_funds(accounts.eve, 2_000_000)
+                .unwrap();
+            assert_eq!(get_balance(accounts.eve), 2_000_000);
+            assert_eq!(smart_contract.amount_stored(), 60_000); // 3% of transaction (2 million)
+            assert_eq!(smart_contract.get_balance(), 98_000_000);
+
+            // send larger funds
+            set_sender(accounts.bob);
+            set_balance(accounts.bob, 0);
+            smart_contract
+                .transfer_funds(accounts.bob, 90_000_000)
+                .unwrap();
+            assert_eq!(get_balance(accounts.bob), 90_000_000);
+            assert_eq!(smart_contract.amount_stored(), 2_760_000); //2.7 mil + 60k (initial)
+            assert_eq!(smart_contract.get_balance(), 8_000_000);
+        }
+
+        #[ink::test]
+        fn test_deduction_overflow_works() {
+            let contract_balance = 100_000_000;
+            let accounts = default_accounts();
+            let mut smart_contract = create_contract(contract_balance);
+            // send initial large funds
+            set_sender(accounts.eve);
+            set_balance(accounts.eve, 0);
+            let transaction = smart_contract.transfer_funds(accounts.eve, 98_000_000);
+
+            assert_eq!(
+                transaction.unwrap_err(),
+                Error::InsufficientFunds {
+                    existential_deposit: 1_000_000,
+                    total_balance: 100_000_000,
+                    potential_balance: 96_060_000,
+                    funds_to_transfer: 98_000_000
+                }
+            );
+        }
+
+        #[ink::test]
+        fn test_claim_funds() {
+            let contract_balance = 100_000_000;
+            let accounts = default_accounts();
+            set_sender(accounts.alice);
+            set_balance(contract_id(), contract_balance);
+            let expiration = 1;
+            let mut smart_contract = DoubleSig::new(expiration);
+            smart_contract
+                .transfer_funds(accounts.bob, 9_000_000)
+                .unwrap();
+            assert_eq!(smart_contract.amount_stored(), 270_000); //3% of 9 mill
+            let total_balance_left = 91_000_000;
+            advance_block();
+            let should_close = move || smart_contract.claim_funds().unwrap();
+            ink::env::test::assert_contract_termination::<ink::env::DefaultEnvironment, _>(
+                should_close,
+                accounts.alice,
+                total_balance_left,
+            );
+        }
     }
 }
